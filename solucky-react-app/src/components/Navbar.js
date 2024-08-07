98f017f0aeade9515bda9befe9fe925fc5e7faf8
Taj Sharma <sharma.tajinder01@gmail.com>
2024-08-06 20:35:58 -0700

    import css fuke and change to heading

--- solucky-react-app/src/components/Navbar.js
+++ solucky-react-app/src/components/Navbar.js
@@ -1,10 +1,11 @@
 import React from "react";  
+import './Navbar.css';
 
 const Navbar = () =>{
     return(
-        <div>
+        <h1>
             Navbar here!
-        </div>
+        </h1>
     );
 }
 
