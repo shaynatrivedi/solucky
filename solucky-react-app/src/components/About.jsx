82fef4411671e6ffd84aae22b1317f0e98855a20
Taj <sharma.tajinder01@gmail.com>
2024-09-06 15:53:49 -0500

    create base files for about page

---
+++ solucky-react-app/src/components/About.jsx
@@ -0,0 +1,14 @@
+// src/components/About.jsx
+import React from 'react';
+import './About.css';
+
+const About = () => {
+  return (
+    <div className="about-page">
+      <h1>About Us</h1>
+      <p>Welcome to the About page. We are a magazine team with a mission to inspire.</p>
+    </div>
+  );
+}
+
+export default About;
