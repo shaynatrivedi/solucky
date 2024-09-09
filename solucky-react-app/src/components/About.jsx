a27c7aa64a7e2e96d09dd13407fc6bd415987867
Taj <sharma.tajinder01@gmail.com>
2024-09-09 13:28:14 -0500

    remove bg div and sectioned the bg into 3 parts

--- solucky-react-app/src/components/About.jsx
+++ solucky-react-app/src/components/About.jsx
@@ -5,9 +5,22 @@ import './About.css';
 const About = () => {
   return (
     <div className="about-page">
-        <div className='about-bg-1'>
+        {/* <div className='about-bg-1'>
             <img src='Assets/About/about-bg-1.png' alt='background 1'/>
-        </div>
+        </div> */}
+
+        <bgsection className='section section1'>
+          <p>we here at section1</p>
+        </bgsection>
+
+        <bgsection className='section section2'>
+          <p>we here at section2</p>
+        </bgsection>
+
+        <bgsection className='section section3'>
+          <p>we here at section3</p>
+        </bgsection>
+
     </div>
   );
 }
