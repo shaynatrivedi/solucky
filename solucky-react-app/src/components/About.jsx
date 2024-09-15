5dca38d00b5496d51086de51521290f67ff85600
Taj <sharma.tajinder01@gmail.com>
2024-09-15 18:27:46 -0500

    Add new divs for textbox

--- solucky-react-app/src/components/About.jsx
+++ solucky-react-app/src/components/About.jsx
@@ -8,17 +8,20 @@ const About = () => {
 
         <section className='section section1'>  
           <div className='about-message founder-message'>
-            <h1>From the Founder</h1>
-            <p>
-            Sanjana Solanki, a Bay Area native & 2024 Business Administration
-            graduate from San Jose State University, created solucky as a creative
-            outlet. Her passions for fashion, film, organization, decoration, & 
-            makeup, along with her South Asian heritage, consistently shape the 
-            brand’s unique vision. Hobbies like thrifting, decorating, & photography 
-            inspired her to create solucky, allowing her to showcase these interests
-            & collaborate with some of the most dedicated & hardworking people she
-            has ever met.
-            </p>
+            <div className='textbox'>
+              <h1>From the Founder</h1>
+              <p>
+              Sanjana Solanki, a Bay Area native & 2024 Business Administration
+              graduate from San Jose State University, created solucky as a creative
+              outlet. Her passions for fashion, film, organization, decoration, & 
+              makeup, along with her South Asian heritage, consistently shape the 
+              brand’s unique vision. Hobbies like thrifting, decorating, & photography 
+              inspired her to create solucky, allowing her to showcase these interests
+              & collaborate with some of the most dedicated & hardworking people she
+              has ever met.
+              </p>
+            </div>
+            
           </div> 
         </section>
 
