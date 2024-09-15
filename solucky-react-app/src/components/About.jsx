10e1be8f4360b5ce601196bf802f2b70bb0185f0
Taj <sharma.tajinder01@gmail.com>
2024-09-15 16:18:02 -0500

    add new div for about page text

--- solucky-react-app/src/components/About.jsx
+++ solucky-react-app/src/components/About.jsx
@@ -5,11 +5,21 @@ import './About.css';
 const About = () => {
   return (
     <div className="about-page">
-        {/* <div className='about-bg-1'>
-            <img src='Assets/About/about-bg-1.png' alt='background 1'/>
-        </div> */}
 
-        <section className='section section1'>   
+        <section className='section section1'>  
+          <div className='about-message founder-message'>
+            <h1>From the Founder</h1>
+            <p>
+            Sanjana Solanki, a Bay Area native & 2024 Business Administration
+            graduate from San Jose State University, created solucky as a creative
+            outlet. Her passions for fashion, film, organization, decoration, & 
+            makeup, along with her South Asian heritage, consistently shape the 
+            brand’s unique vision. Hobbies like thrifting, decorating, & photography 
+            inspired her to create solucky, allowing her to showcase these interests
+            & collaborate with some of the most dedicated & hardworking people she
+            has ever met.
+            </p>
+          </div> 
         </section>
 
         <section className='section section2'>
