9edcc46fdff4efd9ca1ab6f35edffff522f1035a
Taj <sharma.tajinder01@gmail.com>
2024-09-26 14:02:37 -0500

    create div and center the text

--- solucky-react-app/src/components/Team.jsx
+++ solucky-react-app/src/components/Team.jsx
@@ -5,6 +5,20 @@ const Team = () =>{
     return(
         <div className="team-page">
             <section className="team-join">
+                <div className="join-card">
+                    <div className="join-card-text">
+                        <h1>Our Team</h1>
+                        <h3>What does a team contributor do?</h3>
+                        <p>
+                            Our volunteers dedicate their time & effort to making 
+                            the brand what it is today while gaining valuable creative
+                            work experience in their fields of interest.
+                        </p>
+                    </div>
+                    <div className="join-card-buttons">
+                        
+                    </div>
+                </div>
                 
             </section>
 
