3cdd7afc324365d574f15e9bf1e2811e557f7424
Taj <sharma.tajinder01@gmail.com>
2024-09-26 13:45:55 -0500

    change active state of navbar and change navbar color on team page

--- solucky-react-app/src/components/Navbar.jsx
+++ solucky-react-app/src/components/Navbar.jsx
@@ -1,24 +1,28 @@
 import React from "react";  
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import './Navbar.css';
 
 const Navbar = () =>{
+    const location = useLocation();
+    const isTeamPage = location.pathname === '/team'
+
     return(
-        <nav className="navbar">
+        <nav className={`navbar ${isTeamPage ? 'team-page-navbar' : ''}`}>
             <div className="header text-dm-serif">
                 <h1 className="title">solucky</h1>
                 <h3 className="titleCaption"><i>est.2020</i></h3>
             </div>
            
            <ul className="navList">
-            <li><Link to="">shop</Link></li>
-            <li><Link to="">magazine</Link></li>
-            <li><Link to="">media</Link></li>
-            <li><Link to="">collabs</Link></li>
-            <li><Link to="">events</Link></li>
-            <li><Link to="/team">team</Link></li>
-            <li><Link to="/about">about</Link></li>
+            <li><Link to="/shop" className={location.pathname === '/shop' ? 'active':''}>shop</Link></li>
+            <li><Link to="/magazine" className={location.pathname === '/magazine' ? 'active':''}>magazine</Link></li>
+            <li><Link to="/media" className={location.pathname === '/media' ? 'active':''}>media</Link></li>
+            <li><Link to="/collabs" className={location.pathname ==='/collabs' ? 'active':''}>collabs</Link></li>
+            <li><Link to="/events" className={location.pathname === '/events' ? 'active':''}>events</Link></li>
+            <li><Link to="/team" className={location.pathname === '/team' ? 'active':''}>team</Link></li>
+            <li><Link to="/about" className={location.pathname ==='/about' ? 'active':''}>about</Link></li>
            </ul>
+           
         </nav>
     );
 }
