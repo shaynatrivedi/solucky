ad219cf13db18f4648304d44ae285207f1d66f75
Taj Sharma <sharma.tajinder01@gmail.com>
2025-01-15 13:07:41 -0800

    create css and jsx files for the shop page

---
+++ solucky-react-app/src/components/Shop.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import "./Shop.css";
+
+const Shop = () => {
+    return (
+        <div className="shop-page">
+            <section className="section section1">
+                <div className="shop-message">
+                    <div className="textbox">
+                        <h1 className="text-pt-serif">Shop</h1>
+                        <p className="paragraph-message">
+                            Our shop is currently under construction. Please check back soon!
+                        </p>
+                    </div>
+                </div>
+                <img className="shop-image-1" src="../Assets/Shop/1.png" alt="overlap-image"></img>
+            </section>
+        </div>
+    );
+}
