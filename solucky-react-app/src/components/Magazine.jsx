ce2c473bbe7af0280f521785c1ac60ac1d85637f
Taj Sharma <sharma.tajinder01@gmail.com>
2025-01-30 14:51:22 -0800

    add files to put magazine code

---
+++ solucky-react-app/src/components/Magazine.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import './Magazine.css';
+
+
+const Magazine = () =>{
+    return(
+        <div clasName='magazine-page'>
+            HELLO MAGAZINE
+        </div>
+    )
+}
+
+export default Magazine;
