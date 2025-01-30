02839b569b0e6c232074513e4ecebd0e712675f0
Taj Sharma <sharma.tajinder01@gmail.com>
2025-01-30 14:41:53 -0800

    replace footer code with component of Footer

--- solucky-react-app/src/components/Shop.jsx
+++ solucky-react-app/src/components/Shop.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "./Shop.css";
+import Footer from "./Footer";
 
 const Shop = () => {
     return (
@@ -168,81 +169,7 @@ const Shop = () => {
             </div>
           <div>
 
-          <div className="footer">
-            <div className="footer-card num1">
-              <p className="footer-card-title">EXPLORE</p>
-              
-              <div className="explore-links-container">
-                <div className="left-links">
-                  <a className="explore-links" href="www.google.com">home</a>
-                  <a className="explore-links" href="www.google.com">shop</a>
-                  <a className="explore-links" href="www.google.com">magazine</a>
-                  <a className="explore-links" href="www.google.com">media</a>
-                </div>
-
-                <div className="right-links">
-                  <a className="explore-links" href="www.google.com">collabs</a>
-                  <a className="explore-links" href="www.google.com">events</a>
-                  <a className="explore-links" href="www.google.com">team</a>
-                  <a className="explore-links" href="www.google.com">about</a>
-                </div>
-              </div>
-            
-            </div>
-            <div className="footer-card num2">
-              <p className="footer-card-title">JOIN OUR TEAM</p>
-              <a className="join-team-links">apply to become a <i>solucky contributor</i></a>
-              <a className="join-team-links">apply to become a <i>solucky model</i></a>
-            </div>
-
-            <div className="footer-card num3">
-              <p className="footer-card-title">GET IN TOUCH</p>
-              <div className="touch-link-container">
-                <div className="logo-links-div">
-                  <a href="https://stackoverflow.com/questions/42463871/how-to-put-spaces-between-text-in-html">
-                    <img src="Assets/Shop/insta-shop-link.png" alt='insta-logo'></img>
-                  </a>
-
-                  <a href="https://stackoverflow.com/questions/42463871/how-to-put-spaces-between-text-in-html">
-                    <img src="Assets/Shop/spotify-link.png"alt='spotify-logo'></img>
-                  </a>
-
-                  <a href="https://stackoverflow.com/questions/42463871/how-to-put-spaces-between-text-in-html">
-                    <img src="Assets/Shop/linkedin-link.png" alt='linkedin-logo'></img>
-                  </a>
-
-                  <a href="https://stackoverflow.com/questions/42463871/how-to-put-spaces-between-text-in-html">
-                    <img src="Assets/Shop/pinterest-link.png" alt='pinterest-logo'></img>
-                  </a>
-
-                  <a href="https://stackoverflow.com/questions/42463871/how-to-put-spaces-between-text-in-html">
-                    <img src="Assets/Shop/tiktok-link.png" alt='tiktok-logo'></img>
-                  </a>
-                </div>
-
-                <div className="email-link">
-                  solucky.contact@gmail.com
-                </div>
-
-                <div className="join-team-links">
-                click here to subscribe to <i>solucky</i> magazine
-                </div>
-
-              </div>
-            </div>
-
-            <div className="footer-card num4">
-              <p className="footer-card-title">ACKNOWLEDGEMENTS</p>
-              <div className="acknowledgement-container">
-                <p className="position-name"><span className="bold-position">UX/UI Designer:</span> Shayna Trivedi</p>
-                <p className="position-name"><span className="bold-position">Web Developer:</span> Taj Sharma</p>
-                <p className="position-name"><span className="bold-position">Content Curators:</span> Sanjana Solanki & Shayari Saha</p>
-                <p className="position-name"><span className="bold-position">Consultant:</span> Amani Chowdhury</p>
-                <p className="position-name"><span className="bold-position">Brand Photographer & Photo Editor:</span> Amrutha Gururaja</p>
-              </div>
-            </div>
-
-          </div>
+          <Footer/>
           </div>
         </div>
     );
