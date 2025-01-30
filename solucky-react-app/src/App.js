42a04625efa45f8fc90b46b2d11a7f20f50c73d0
Taj Sharma <sharma.tajinder01@gmail.com>
2025-01-30 14:40:39 -0800

    add magazin path

--- solucky-react-app/src/App.js
+++ solucky-react-app/src/App.js
@@ -4,6 +4,7 @@ import About from './components/About';
 import Home from './components/Home';
 import Team from './components/Team';
 import Shop from './components/Shop';
+import Magazine from './components/Magazine';
 import './components/Navbar.css';
 import './App.css';
 
@@ -17,6 +18,7 @@ function App() {
           <Route path="/about" element={<About/>} />
           <Route path="/team" element={<Team/>} />
           <Route path="/shop" element={<Shop/>} />
+          <Route path="/magazine" element={<Magazine/>} />
         </Routes>
 
       </div>
