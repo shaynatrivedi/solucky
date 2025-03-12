f2231bdf4b94d82e2767128b9f5fd845df7f3eba
Taj Sharma <sharma.tajinder01@gmail.com>
2025-03-12 10:19:10 -0700

    add html to place items in the relative postion in the dom

--- solucky-react-app/src/components/Magazine.jsx
+++ solucky-react-app/src/components/Magazine.jsx
@@ -4,9 +4,45 @@ import './Magazine.css';
 
 const Magazine = () =>{
     return(
-        <div clasName='magazine-page'>
-            HELLO MAGAZINE
-        </div>
+        <section className="magazine-page">
+            <div className="magazine-info-div">
+                <div className="info-text-container">
+                    <div className="top-text-container">
+                        <div className="header-text-container">
+                            <p className="header-brand-text">solucky</p>
+                            <p className="header-magazine-text">maga</p>
+                            <p className="header-magazine-text">zine</p>
+                        </div>
+                        <div className="magazine-para-text" id="text-box">
+                            At first, solucky began as a blog to share the founder’s passions with family & friends, 
+                            where makeup, fashion, organization, decoration, & film intertwined. The blog soon gained 
+                            popularity & blossomed into a multifaceted brand thanks to the endless support & 
+                            collaboration of the community. In just a year, solucky evolved from a blog into a magazine,
+                            fueled by contributions from a devoted team of volunteers. 
+                        </div>
+                    </div>
+
+                    <div className="bottom-text-coontainer" >
+                        <div className="magazine-para-text">
+                            Since its first magazine issue in March 2021, solucky has become a publication where every photo, 
+                            video, article, & piece of art is infused with the dedication of its contributors. The brand became
+                            a platform for a growing community of creatives, each contributing their passions & skills to build 
+                            something greater than any one individual. Operating on no budget, each issue is a testament to the 
+                            power of collaboration & community, with a unique theme & a space where creativity is not only shared 
+                            but celebrated.
+                        </div>
+                    </div>
+
+                </div>
+                <div className="info-button-containter">
+                    buttons!
+                </div>
+            </div>
+
+            <img className="right-sticky-picture" src="../Assets/Magazine/legs.png" alt="legs"></img>
+        
+
+        </section>       
     )
 }
 
