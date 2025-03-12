17d9732e2f03f5ec3f3b0ef6765d348e7861b966
Taj Sharma <sharma.tajinder01@gmail.com>
2025-03-12 13:28:33 -0700

    replace image and change color of button to black

--- solucky-react-app/src/components/Team.jsx
+++ solucky-react-app/src/components/Team.jsx
@@ -28,7 +28,7 @@ const Team = () =>{
                         <a href="https://www.youtube.com/watch?v=xvFZjo5PgG0">
                             <button type="button">
                                 <p className="apply-textbox centered-p-bai-jamjuree">view bts photos</p>
-                                <img src="/Assets/Team/apply-arrow.png" alt="arrow"></img>
+                                <img className="button-arrow" src="../Assets/Icons/shop-arrow.png" alt="arrow"></img>
                             </button>
                         </a>
                     </div>
@@ -38,14 +38,14 @@ const Team = () =>{
                             <a href="https://docs.google.com/forms/d/e/1FAIpQLSe81g9uPNPingKHngREZ5MpHuUFtI-zXEeOLqVayNIC6Ba2Sg/viewform">
                                 <button type="button">
                                     <p className="apply-textbox centered-p-bai-jamjuree">apply to be a <i>solucky</i> contributor</p>
-                                    <img src="/Assets/Team/apply-arrow.png" alt="arrow"></img>
+                                    <img className="button-arrow" src="../Assets/Icons/shop-arrow.png" alt="arrow"></img>
                                 </button>
                             </a>
 
                             <a href="https://docs.google.com/forms/d/e/1FAIpQLSfZog6msmIUc8K_dc2SsmXIWx7oEGcV4E1PE9WHw0KxKNjEwg/viewform">
                                 <button type="button">
                                     <p className="apply-textbox centered-p-bai-jamjuree">apply to be a <i>solucky</i> model</p>
-                                    <img src="/Assets/Team/apply-arrow.png" alt="arrow"></img>
+                                    <img className="button-arrow" src="../Assets/Icons/shop-arrow.png" alt="arrow"></img>
                                 </button>
                             </a>
                         </div>
