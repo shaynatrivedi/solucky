473df23fd5cfd8952829ef3d788be2928257d31d
Taj <sharma.tajinder01@gmail.com>
2025-04-08 21:22:07 -0500

    place content for the top section on html file

--- solucky-react-app/src/components/Collabs.jsx
+++ solucky-react-app/src/components/Collabs.jsx
@@ -4,8 +4,44 @@ import Footer from './Footer';
 
 const Collabs = () =>{
     return(
-        <div>
-            HI FROM COLLAB PAGE
+        <div className="collabs-page">
+            <div className="top-section">
+                <div className="content-container-grid">
+                    <div className="create-grid-div">
+                        <div className="collab-bigger-text">C</div>
+                        <div className="collab-big-text">reate</div>
+                        <div className="collab-fancy-text">with solucky</div>
+                    </div>
+                    <div className="collab-paragraph-container">
+                        <div className="magazine-para-text">
+                            A collaboration with solucky creates a seamless partnership 
+                            between your business & our creative brand. Simply send us 
+                            your products, & our team of experts including models, 
+                            production crew, & creative directors will take care of the
+                            rest. After the shoot(s), we’ll return your products along
+                            with high-quality content that you can freely use across 
+                            your social media platforms.
+                        </div>
+                        <div className="magazine-para-text">
+                            Additionally, this content will be featured on our social 
+                            media channels & website, expanding your reach & boosting 
+                            engagement. All that we ask in return is that you credit our 
+                            brand & team, along with our collaboration title. If desired, 
+                            we’re happy to collaborate on the creative direction, ensuring 
+                            a vision both parties love.
+                        </div>
+                        <div className="magazine-para-text">
+                            Email solucky.contact@gmail.com to collab with us!
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div className="bottom-section">
+
+            </div>
+
+            
         </div>
     )
 }
